a95c57c9df4a59cdd1412e638e4cb9105207a281
Adithya Venkatesh Mohan <mohanah@mail.uc.edu>
2019-07-11 20:05:39 -0400

    the robot description is modfied so that the laser doesn't the ground as the min vertical angle is changed to 0 radians /* *sonar sim was interfaced 3d points and echoes were generated and plotted in matplot/ to Do:::::::/ yet have publish the echoes and time in a msg format nearest obstacle and collision detection needed to implemented for Reward function Reward function has to be designed

--- mybot_ws/src/mybot_sonar/src/sonar_gen.py
+++ mybot_ws/src/mybot_sonar/src/sonar_gen.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 
 # topic that need to be subcribed is a point cloud /mybot/laser/pointcloud2
@@ -17,6 +17,14 @@ from geometry_msgs.msg import Point
 import sensor_msgs.point_cloud2 as pc2
 
 
+import numpy
+from matplotlib import pyplot
+from scipy.interpolate import interp1d
+import library
+
+
+
+
 #callback on reciving the pointcloud data
 def callback(data):
     
@@ -28,13 +36,85 @@ def callback(data):
     
     pc_list = data.points
     intensity_list = data.channels
-    rospy.loginfo(rospy.get_caller_id()+"points are " + "data")
-    rospy.loginfo(pc_list[1])
-    p = pc_list[1].x
-    val = intensity_list[0].name
-    value = intensity_list[0]
-    rospy.loginfo("x point")
-    rospy.loginfo(value)
+
+    #param for the echo generation
+    sample_frequency = 250000
+    emission_level = 100
+    emission_duration = 0.0025
+    emission_frequency = 40000
+    emitter_radius = 0.005
+    absorption_coefficient = 1.318  # http://www.sengpielaudio.com/calculator-air.htm
+    reflection_strength = -40
+    speed_of_sound = 340
+
+    ##### this is where i can give the input 
+    azimuths = numpy.array([])
+    elevations = numpy.array([])
+    distances = numpy.array([])
+    print("single point", pc_list[0])
+    for p in pc_list:
+        #print(p)
+        distances = numpy.append(distances, p.x)
+        azimuths = numpy.append(azimuths, p.y)
+        elevations = numpy.append(elevations, p.z)
+
+    # if not we didn't recive any points    
+    assert(distances.size > 0)
+
+
+    emission_samples = int(sample_frequency * emission_duration)
+    emission_time = numpy.linspace(0, emission_duration, emission_samples)
+    emission = numpy.sin(2 * numpy.pi * emission_frequency * emission_time)
+    window = library.signal_ramp(emission_samples, 10)
+    emission = emission * window
+
+    # pyplot.figure()
+    # pyplot.plot(emission)
+
+    # %%
+    # Make piston model (model of emitter directionality)
+
+    piston, degrees = library.pistonmodel(emission_frequency, radius=emitter_radius)
+    piston = 10 * numpy.log10(piston)
+    piston_function = interp1d(degrees, piston)
+
+    # pyplot.figure()
+    # pyplot.plot(degrees, piston)
+
+    # %%
+    # Get excentricities and echo delays
+
+    excentricities = library.gca(azimuths, elevations, 0, 0)
+    delays = 2 * distances / speed_of_sound
+
+    # %%
+    # Calculate path losses
+
+    loss_directionality = piston_function(excentricities)
+    loss_attenuation = - 2 * distances * absorption_coefficient
+    loss_spreading = -40 * numpy.log10(distances)
+
+    echoes = emission_level + reflection_strength + loss_directionality + loss_attenuation + loss_spreading
+    echoes_pa = library.db2pa(echoes)
+    echoes_pa[echoes < 0] = 0
+
+    # %%
+    # Make impulse response
+
+    impulse_time, impulse_response = library.make_impulse_response(delays, echoes_pa, emission_duration, sample_frequency)
+
+    # pyplot.figure()
+    # pyplot.plot(impulse_time, impulse_response)
+
+    # %%
+    # Make echo sequence
+
+    echo_sequence = numpy.convolve(emission, impulse_response, mode='same')
+
+    pyplot.figure()
+    pyplot.plot(impulse_time, echo_sequence)
+
+    pyplot.show()
 
 
 
@@ -44,7 +124,7 @@ def listener3d():
     #creates  a node called peeker to process the 
     rospy.init_node("peeker", anonymous = True)
     #subscribing to the pointcloud data which is published  
-    rospy.Subscriber("/mybot/laser/pointcloud", PointCloud, callback)
+    rospy.Subscriber("/mybot/laser/polorcloud", PointCloud, callback)
     rospy.spin() #keeps the python from exciting until the node stopped
 
 
@@ -55,3 +135,7 @@ if __name__ == "__main__":
     except rospy.ROSInterruptException:
         pass
 
+
+
+    
+    
